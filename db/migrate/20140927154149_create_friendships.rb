d373ef465f0a5a32c8846d1034cdc41f1bd76c3d
Brandon Hopkins <brhpkns@gmail.com>
2014-09-27 13:05:43 -0400

    Bootstrap installed and header added

--- db/migrate/20140927154149_create_friendships.rb
+++ db/migrate/20140927154149_create_friendships.rb
@@ -1,6 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
+      t.integer :user_id
+      t.integer :friend_id
+      t.index :user_id
 
       t.timestamps
     end
--- db/migrate/20140927154203_create_notifications.rb
+++ db/migrate/20140927154203_create_notifications.rb
@@ -1,6 +1,10 @@
 class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
+      t.string :text
+      t.integer :sender_id
+      t.integer :recipient_id
+      t.boolean :unread?
 
       t.timestamps
     end
