fc1de55dd3e31beef00e084a1ffe4dc339a616b1
Brandon Hopkins <brhpkns@gmail.com>
2014-09-29 17:14:01 -0400

    Available friends list on visiting feed page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+brandon = User.create(screen_name: "Brandon", email: "bh@foo.com", password: "password1234", password_confirmation: "password1234", status: "available")
+peter = User.create(screen_name: "Peter", email: "pg@foo.com", password: "password1234", password_confirmation: "password1234", status: "available")
+william = User.create(screen_name: "William", email: "wl@foo.com", password: "password1234", password_confirmation: "password1234", status: "unavailable")
+jessica = User.create(screen_name: "Jessica", email: "jr@foo.com", password: "password1234", password_confirmation: "password1234", status: "available")
+vanessa = User.create(screen_name: "Vanessa", email: "vd@foo.com", password: "password1234", password_confirmation: "password1234", status: "unavailable")
+
+brandon.friends << [peter, william, jessica, vanessa]
+peter.friends << [brandon]
+william.friends << [brandon, vanessa]
+jessica.friends << [brandon, vanessa]
+vanessa.friends << [brandon, william, jessica]
